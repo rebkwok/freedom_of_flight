878e7f061e09fc41bc2287b3fa89ee40ed10686d
Becky Smith <rebkwok@gmail.com>
2020-07-11 11:38:00 +0100

    Remove some unused accounts code

--- accounts/templatetags/accounttags.py
+++ accounts/templatetags/accounttags.py
@@ -4,14 +4,6 @@ from ..models import has_active_disclaimer, has_expired_disclaimer as has_expire
 
 register = template.Library()
 
-
-@register.simple_tag
-def modify_redirect_field_value(ret_url):
-    if ret_url and ret_url in ['/accounts/password/change/', '/accounts/password/set/']:
-        return '/accounts/profile'
-    return ret_url
-
-
 @register.filter
 def has_disclaimer(user):
     return has_active_disclaimer(user)
