fc69ce3c88044598447fb6d9f764986ec619fdb6
Becky Smith <rebkwok@gmail.com>
2020-07-13 17:02:25 +0100

    More template tweaks

--- booking/templatetags/bookingtags.py
+++ booking/templatetags/bookingtags.py
@@ -3,7 +3,7 @@ from django import template
 from ..models import WaitingListUser
 from ..utils import has_available_block as has_available_block_util
 from ..utils import has_available_course_block as has_available_course_block_util
-from ..utils import get_active_user_block, get_block_status, user_can_book_or_cancel
+from ..utils import get_block_status, user_can_book_or_cancel
 
 register = template.Library()
 
@@ -17,20 +17,6 @@ def has_available_course_block(user, course):
     return has_available_course_block_util(user, course)
 
 
-# @register.filter
-# def block_used(user, event):
-#     block = get_active_user_block(user, event)
-#     if event.course:
-#         # Don't show the used/total for course blocks
-#         if user.bookings.filter(event=event, status="OPEN", no_show=False).exists():
-#             return f"Block used"
-#         return f"Block available"
-#
-#     if user.bookings.filter(event=event, status="OPEN", no_show=False).exists():
-#         return f"Block used"
-#     return f"Block available"
-
-
 def get_block_info(block):
     used, total = get_block_status(block)
     base_text = f"<span class='helptext'>{block.user.first_name} {block.user.last_name}: {block.block_config.identifier} ({total - used}/{total} remaining)"
@@ -40,14 +26,6 @@ def get_block_info(block):
         return f"{base_text}; not started</span>"
     else:
         return f"{base_text}; never expires</span>"
-
-# @register.filter
-# def block_info(user, event):
-#     block = get_active_user_block(user, event)
-#     if event.course:
-#         # Don't show the used/total for course blocks
-#         return f"<span class='helptext'>{block.block_config.identifier} </span>"
-#     return get_block_info(block)
 
 
 @register.filter
@@ -88,16 +66,6 @@ def block_expiry_text(block):
 @register.filter
 def can_book_or_cancel(user, event):
     return user_can_book_or_cancel(user, event)
-
-
-# @register.filter
-# def has_open_or_cancelled_booking(user, event):
-#     return user.bookings.filter(event=event).exists()
-
-
-# @register.filter
-# def has_course_booking(user, event):
-#     return user.bookings.filter(event=event).exists()
 
 
 @register.filter
