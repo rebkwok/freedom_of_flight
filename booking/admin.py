14973c5ee338e80a27df34f66da6a78fd57d50ad
Becky Smith <rebkwok@gmail.com>
2020-09-02 15:03:15 +0100

    Admin updates

--- booking/admin.py
+++ booking/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
+from common.utils import full_name
+
 from .models import (
     Block, BlockConfig, Booking, BlockVoucher, GiftVoucherType,
     Course, Event, EventType, Track, WaitingListUser, SubscriptionConfig, Subscription
@@ -34,9 +36,29 @@ class TrackAdmin(admin.ModelAdmin):
     inlines = (EventTypeInline,)
 
 
+class BookingAdmin(admin.ModelAdmin):
+    model = Booking
+    list_filter = ["event"]
+    list_display = ["get_user", "event", "status", "no_show", "block"]
+    search_fields = ["user__first_name", "user__last_name"]
+
+    def get_user(self, obj):
+        return full_name(obj.user)
+    get_user.short_description = "User"
+    get_user.admin_order_field = 'user__first_name'
+
+
 class BlockAdmin(admin.ModelAdmin):
     model = Block
     inlines = (BookingInline,)
+    list_filter = ["user"]
+    list_display = ["get_user", "block_config", "start_date", "expiry_date", "paid"]
+    search_fields = ["user__first_name", "user__last_name"]
+
+    def get_user(self, obj):
+        return full_name(obj.user)
+    get_user.short_description = "User"
+    get_user.admin_order_field = 'user__first_name'
 
 
 class EventAdmin(admin.ModelAdmin):
@@ -58,7 +80,7 @@ admin.site.site_header = "Freedom of Flight Admin"
 admin.site.register(Event, EventAdmin)
 admin.site.register(Track, TrackAdmin)
 admin.site.register(Course, CourseAdmin)
-admin.site.register(Booking)
+admin.site.register(Booking, BookingAdmin)
 admin.site.register(Block, BlockAdmin)
 admin.site.register(BlockConfig)
 admin.site.register(EventType)
