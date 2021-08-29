0a46fa31f60048a3c6a37344f97c4fae0c2dab36
Becky Smith <rebkwok@gmail.com>
2021-08-29 13:17:16 +0100

    Fixes after upgrading packages

--- payments/forms.py
+++ payments/forms.py
@@ -7,8 +7,9 @@ from paypal.standard.forms import PayPalPaymentsForm
 class PayPalPaymentsFormWithId(PayPalPaymentsForm):
 
     def render(self):
-        return format_html("""<form id="go_to_paypal_form" action="{0}" method="post">
-    {1}
-    <input id="submit_paypal" type="image" src="#" name="submit" alt="Buy it Now" style="display:none"/>
-    <span class="btn btn-sm btn-primary mb-1"><i class="fa fa-spinner fa-spin"></i> Redirecting to PayPal</span>
-</form>""", self.get_endpoint(), self.as_p())
+        return format_html(
+            """<form id="go_to_paypal_form" action="{0}" method="post">
+            {1}<input id="submit_paypal" type="image" src="#" name="submit" alt="Buy it Now" style="display:none"/>
+            <span class="btn btn-sm btn-primary mb-1"><i class="fa fa-spinner fa-spin"></i> Redirecting to PayPal</span>
+            </form>""", self.get_login_url(), self.as_p()
+       )
